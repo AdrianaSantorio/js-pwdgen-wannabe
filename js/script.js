6f4a4f186e8b36bbd035dc6d6c40281433cbe4c5
AdrianaSantorio <adriana.santorio@gmail.com>
2021-12-20 16:07:07 +0100

    added prompt for "password"

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-console.log ('JS OK')
+rconsole.log ('JS OK')
 
 // Chiedi all’utente il suo nome
 // chiedi il suo cognome,
 // chiedi il suo colore preferito
+// crea la  password
+// recupera un elemento dalla pagina
+
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
 const nome = prompt('Inserire Nome');
@@ -12,3 +15,11 @@ console.log(nome);
 const cognome = prompt('Inserire Cognome');
 
 console.log(cognome);
+
+const colore = prompt("qual è il tuo colore preferito?");
+
+console.log(colore)
+
+const password = nome + cognome + colore + '21';
+
+console.log(password)
